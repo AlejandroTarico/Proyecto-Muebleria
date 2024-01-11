5477baeeb6c7b2f86a2502eefc17075bf1028d4b
alejoT <yo.alejo.at@gmail.com>
2024-01-11 03:20:05 -0300

    Provando que se este subiendo bien los cambios

--- Front Muebles/src/Components/NavBar/NavBar.jsx
+++ Front Muebles/src/Components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
   return (
     <div className='col-span-10 pt-1'>
         <ul className='list-none flex justify-around'>
-            <li><NavLink  to={ROUTES.HOME}>Inicio</NavLink></li>
+            <li><NavLink  to={ROUTES.HOME}>Inicio</NavLink></li> 
             <li><NavLink  to={ROUTES.NUESTROSMUEBLES}>Nuestros Muebles</NavLink></li>
             <li><NavLink  to={ROUTES.ABOUT}>Quienes Somos</NavLink></li>
             <li><NavLink  to={ROUTES.MICOMBO}>Mi Combo</NavLink></li>
